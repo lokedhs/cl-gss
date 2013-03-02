6269b3055e5e1ec020e871395eca51e21466dbfe
Elias Martenson <lokedhs@gmail.com>
2013-03-02 21:56:13 +0800

    Further development

--- src/cl-gss.lisp
+++ src/cl-gss.lisp
@@ -2,5 +2,76 @@
 
 (declaim #.*compile-decl*)
 
-(defun gss-init-sec-context (x)
-  nil)
+(cffi:define-foreign-library libgssapi
+  (:darwin "libgssapi_krb5.dylib")
+  (:unix (:or "libgssapi_krb5.so" "libgss.so")))
+
+(cffi:use-foreign-library libgssapi)
+
+(cffi:defcfun ("gss_import_name" gss-import-name) om-uint32
+  (minor-status (:pointer om-uint32))
+  (input-name-buffer (:pointer gss-buffer-desc))
+  (input-name-type gss-oid)
+  (output-name (:pointer gss-name-t)))
+
+(cffi:defcfun ("gss_display_name" gss-display-name) om-uint32
+  (minor-status (:pointer om-uint32))
+  (name gss-name-t)
+  (output-name (:pointer gss-buffer-desc))
+  (output-type (:pointer gss-oid)))
+
+(cffi:defcfun ("gss_display_status" gss-display-status) om-uint32
+  (minor-status (:pointer om-uint32))
+  (status-value-input om-uint32)
+  (status-type :int)
+  (mech-type gss-oid)
+  (message-context (:pointer om-uint32))
+  (status-string (:pointer gss-buffer-desc)))
+
+(cffi:defcfun ("gss_release_buffer" gss-release-buffer) om-uint32
+  (minor-status (:pointer om-uint32))
+  (buffer (:pointer gss-buffer-desc)))
+
+(cffi:defcvar ("GSS_C_NT_HOSTBASED_SERVICE" *gss-c-nt-hostbased-service* :read-only t) gss-oid)
+(cffi:defcvar ("GCC_C_NO_OID" *gss-c-no-oid* :read-only t) gss-oid)
+
+(defun make-name (name-string)
+  (let ((output-name (cffi:foreign-alloc 'gss-name-t)))
+    (cffi:with-foreign-string (foreign-name-string name-string)
+      (cffi:with-foreign-objects ((minor 'om-uint32)
+                                  (buf 'gss-buffer-desc))
+        (setf (cffi:foreign-slot-value buf 'gss-buffer-desc 'length) (1+ (length name-string)))
+        (setf (cffi:foreign-slot-value buf 'gss-buffer-desc 'value) foreign-name-string)
+        (let ((status (gss-import-name minor
+                                       buf
+                                       *gss-c-nt-hostbased-service*
+                                       output-name)))
+          (format t "status = ~s, minor = ~s~%" status (cffi:mem-ref minor 'om-uint32)))
+        output-name))))
+
+(defun name-to-string (name)
+  (cffi:with-foreign-objects ((minor 'om-uint32)
+                              (output-name 'gss-buffer-desc)
+                              (output-type 'gss-oid))
+    (let ((status (gss-display-name minor (cffi:pointer-address name) output-name output-type)))
+      (format t "status=~s, minor=~s, errors=~s~%"
+              status (cffi:mem-ref minor 'om-uint32) (errors-as-string status minor)))))
+
+(defun errors-as-string (major-status minor-status)
+  (declare (ignore minor-status))
+  (unless (zerop major-status)
+    (cffi:with-foreign-objects ((message-context 'om-uint32))
+      (loop
+         collect (cffi:with-foreign-objects ((minor 'om-uint32)
+                                             (status-output 'gss-buffer-desc))
+                   (let ((status (gss-display-status minor major-status gss-c-gss-code gss-c-no-oid
+                                                     message-context status-output)))
+                     (unwind-protect
+                          (progn
+                            (unless (zerop status)
+                              (error "call to gss-display-status failed with status=~s" status))
+                            (cffi:convert-from-foreign (cffi:foreign-slot-value status-output
+                                                                                'gss-buffer-desc
+                                                                                'value) :string))
+                       (gss-release-buffer minor status-output))))
+         until (zerop (cffi:mem-ref message-context 'om-uint32))))))
--- src/gss-grovel.lisp
+++ src/gss-grovel.lisp
@@ -2,5 +2,20 @@
 
 (include "gssapi/gssapi.h")
 
-(ctype :om-uint32 "OM_uint32")
-;(cstruct :gss-buffer-desc "gss_buffer_desc" (:length "length") (:value "value"))
+(constant (gss-c-gss-code "GSS_C_GSS_CODE"))
+(constant (gss-c-no-name "GSS_C_NO_NAME"))
+(constant (gss-c-no-buffer "GSS_C_NO_BUFFER"))
+(constant (gss-c-no-oid "GSS_C_NO_OID"))
+(constant (gss-c-no-oid-set "GSS_C_NO_OID_SET"))
+(constant (gss-c-no-context "GSS_C_NO_CONTEXT"))
+(constant (gss-c-no-credential "GSS_C_NO_CREDENTIAL"))
+(constant (gss-c-no-channel-bindings "GSS_C_NO_CHANNEL_BINDINGS"))
+
+(ctype om-uint32 "OM_uint32")
+
+(cstruct gss-buffer-desc "gss_buffer_desc" (length "length" :type om-uint32) (value "value" :type :pointer))
+(ctype gss-buffer-t "gss_buffer_t")
+
+(ctype gss-name-t "gss_name_t")
+
+(ctype gss-oid "gss_OID")
