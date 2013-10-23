c3c292a582b7feb1bc09a14bf76711531acc2efc
Elias Martenson <lokedhs@gmail.com>
2013-10-23 14:15:27 +0800

    Updated the docstring for MAKE-NAME

--- src/cl-gss.lisp
+++ src/cl-gss.lisp
@@ -109,6 +109,9 @@ actual registration of the object is handled by the subclass."))
 
 (defun make-name (name-string &key (type :hostbased-service))
   "Create a new name object representing the given name.
+The TYPE parameter indicates the requested usage type, its
+value is one of :USER-NAME, :MACHINE-UID-NAME, :STRING-UID-NAME,
+:HOSTBASE-SERVICE.
 This function implements the functionality of the GSSAPI
 function `gss_import_name'."
   (check-type name-string string)
